0a5d83a301e610369fa959800880bc3f851253ad
catamo <victor.catamo.rojas@gmail.com>
2016-04-29 18:49:04 -0430

    Overlay para loading agregado

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-$(window).scroll(function(e){
-  parallax();
+$(window).ready(function () {
+  $(".overlay").fadeOut(999);
+  $(".preloadBG").each(function () {
+    var t, n, r = $(this),
+    i = r.css("background-image");
+    i && (t = i.replace(/(^url\()|(\)$|[\"\'])/g, ""),
+    n = new Image, n.src = t, n.complete && $(n).trigger("load"))
+  });
 });
 
-function parallax(){
-  var scrolled = $(window).scrollTop(),
-      percentage = scrolled / $(document).height();
-      $('.background').css('background-position','center ' + -((percentage * 15) - 50)+'%');
-}
+var imgCount = $("img").length,
+currentImgCount = 0;
 
-$("html,body").scrollTop(0);
-$(window).ready(function() {
-  $('.background').css('background-size','110%');
-  $('.onload').addClass('active');
-  $("html,body").animate({scrollTop: 0}, 100);
-});
+$("img").load(function () {
+  currentImgCount++;
+  currentImgCount == imgCount && setTimeout(function () {
+    /mobile/i.test(navigator.userAgent) && !location.hash && setTimeout(function () {
+      $("img").unbind("load");
+    },
+    2e3)
+  }).each(function () { this.complete && $(this).trigger("load") });
+  });
+
+
+  $(window).scroll(function(e){
+    parallax();
+  });
+
+  function parallax(){
+    var scrolled = $(window).scrollTop(),
+    percentage = scrolled / $(document).height();
+    $('.background').css('background-position','center ' + -((percentage * 15) - 50)+'%');
+  }
+
+  $("html,body").scrollTop(0);
+  $(window).ready(function() {
+    $('.background').css('background-size','110%');
+    $('.onload').addClass('active');
+    $("html,body").animate({scrollTop: 0}, 100);
+  });
