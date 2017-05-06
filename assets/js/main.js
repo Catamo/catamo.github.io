045560455c440e7cad8b9d5afaad11511d368a65
catamo <victor.catamo.rojas@gmail.com>
2017-05-06 12:07:43 -0400

    Gustavotaliz images changed. Lightbox issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,7 +249,7 @@
 							href = $a.attr('href');
 
 						// Not an image? Bail.
-							if (!href.match(/\.(jpg|gif|png|mp4)$/))
+							if (!href.match(/\.(jpg|JPG|gif|GIF|png|PNG|mp4|MP4)$/))
 								return;
 
 						// Prevent default.
